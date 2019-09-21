e07c4b8633b6dfac81dc3ba375e79c0d46d83b77
aldifirmansyah <aldifirmansyh@gmail.com>
2019-09-21 21:08:21 +0700

    [ADD] Komoditas Hortikultura section done

--- komoditas-hortikultura.js
+++ komoditas-hortikultura.js
@@ -1,8 +1,10 @@
 const widthBarChart = 500;
 const heightBarChart = 220;
 const marginBarChart = 10;
+const xAxisSpace = 20;
 const barPadding = 4;
 const hortikulturaData = [
+  { period: "Jan 2017", data: [7, 14, 19, 22, 15] },
   { period: "Feb 2017", data: [4, 20, 15, 30, 10] },
   { period: "Mar 2017", data: [10, 23, 7, 19, 11] },
   { period: "Apr 2017", data: [3, 19, 17, 32, 14] },
@@ -28,21 +30,116 @@ const biggestDataSum = hortikulturaData.reduce((acc, next) => {
 
 let barWidth =
   (widthBarChart - 2 * marginBarChart) / hortikulturaData.length - barPadding;
-let heightPerCount = (heightBarChart - 2 * marginBarChart) / biggestDataSum;
+let heightPerCount =
+  (heightBarChart - 2 * marginBarChart - xAxisSpace) / biggestDataSum;
 
 let svg = d3
   .select("#bar-chart-svg")
   .attr("width", widthBarChart)
   .attr("height", heightBarChart);
 
-let barChart = svg.selectAll("rect").data(hortikulturaData);
-barChart
-  .enter()
+let dataBarChart = svg.selectAll("g").data(hortikulturaData);
+let groupBarChart = dataBarChart.enter().append("g");
+
+groupBarChart
   .append("rect")
+  .classed("first-color-bar", true)
+  .attr("x", (d, i) => (barWidth + barPadding) * i + marginBarChart)
+  .attr("y", d => heightBarChart - marginBarChart - d.data[0] - xAxisSpace)
+  .attr("height", d => d.data[0] * heightPerCount)
+  .attr("width", barWidth);
+groupBarChart
+  .append("rect")
+  .classed("second-color-bar", true)
   .attr("x", (d, i) => (barWidth + barPadding) * i + marginBarChart)
   .attr(
     "y",
-    d => heightBarChart - marginBarChart - sum(d.data) * heightPerCount
+    d =>
+      heightBarChart -
+      marginBarChart -
+      sum([...d.data].splice(0, 2)) -
+      xAxisSpace
   )
-  .attr("height", d => sum(d.data) * heightPerCount)
+  .attr("height", d => d.data[1] * heightPerCount)
   .attr("width", barWidth);
+groupBarChart
+  .append("rect")
+  .classed("third-color-bar", true)
+  .attr("x", (d, i) => (barWidth + barPadding) * i + marginBarChart)
+  .attr(
+    "y",
+    d =>
+      heightBarChart -
+      marginBarChart -
+      sum([...d.data].splice(0, 3)) -
+      xAxisSpace
+  )
+  .attr("height", d => d.data[2] * heightPerCount)
+  .attr("width", barWidth);
+groupBarChart
+  .append("rect")
+  .classed("fourth-color-bar", true)
+  .attr("x", (d, i) => (barWidth + barPadding) * i + marginBarChart)
+  .attr(
+    "y",
+    d =>
+      heightBarChart -
+      marginBarChart -
+      sum([...d.data].splice(0, 4)) -
+      xAxisSpace
+  )
+  .attr("height", d => d.data[3] * heightPerCount)
+  .attr("width", barWidth);
+groupBarChart
+  .append("rect")
+  .classed("fifth-color-bar", true)
+  .attr("x", (d, i) => (barWidth + barPadding) * i + marginBarChart)
+  .attr("y", d => heightBarChart - marginBarChart - sum(d.data) - xAxisSpace)
+  .attr("height", d => d.data[4] * heightPerCount)
+  .attr("width", barWidth);
+
+groupBarChart
+  .append("text")
+  .text(d => sum(d.data))
+  .attr(
+    "x",
+    (d, i) => (barWidth + barPadding) * i + barWidth * 0.5 + marginBarChart
+  )
+  .attr(
+    "y",
+    d => heightBarChart - marginBarChart - sum(d.data) - 2 - xAxisSpace
+  )
+  .attr("text-anchor", "middle");
+
+let xAxis = groupBarChart.append("g").classed("x-axis", true);
+
+xAxis
+  .append("text")
+  .text(d => d.period.split(" ")[0])
+  .attr(
+    "x",
+    (d, i) => (barWidth + barPadding) * i + barWidth * 0.5 + marginBarChart
+  )
+  .attr("y", heightBarChart - 20)
+  .attr("text-anchor", "middle");
+xAxis
+  .append("text")
+  .text(d => d.period.split(" ")[1])
+  .attr(
+    "x",
+    (d, i) => (barWidth + barPadding) * i + barWidth * 0.5 + marginBarChart
+  )
+  .attr("y", heightBarChart - 8)
+  .attr("text-anchor", "middle");
+
+// let barChart = svg.selectAll("rect").data(hortikulturaData);
+// barChart
+//   .enter()
+//   .append("rect")
+//   .attr("x", (d, i) => (barWidth + barPadding) * i + marginBarChart)
+//   .attr(
+//     "y",
+//     d => heightBarChart - marginBarChart - sum(d.data) * heightPerCount
+//   )
+//   .attr("height", d => sum(d.data) * heightPerCount)
+//   .attr("width", barWidth);
